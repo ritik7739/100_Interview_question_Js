497d2514613d76306e79dd1bf8f9bcdafa3532ce
ritik7739 <ritik.kumar7739@gmail.com>
2025-07-15 15:35:42 +0530

    Update the interview question for promise

--- 1.promise.all.js
+++ 1.promise.all.js
@@ -1,21 +1,48 @@
 const promise1=new Promise((resolve,reject)=>{
-    resolve(123);
+    setTimeout(()=>{
+        resolve(123);
+    },2000);
 });
 
 const promise2=new Promise((resolve,reject)=>{
-    resolve('I am called');
+    setTimeout(()=>{
+        resolve('I am called');
+    },3000);
 });
 
 const promise3=new Promise((resolve,reject)=>{
-     reject("I am rejected")
+     setTimeout(()=>{
+        reject("I am rejected");
+    },4000);
 });
 
 
-// Promise.all([promise1,promise2,promise3]).then((res)=>{
-//     console.log(res);
-// }).catch((err)=>{
-//     console.log(err);
-// });
+//promise.all()
+// It will return an array of results in the same order as the promises were passed
+// If any promise is rejected, it will reject the entire Promise.all
+// It will not wait for all promises to resolve if one is rejected
+// It will return a promise that resolves when all of the promises in the iterable have resolved or
+
+Promise.all([promise1,promise2,promise3]).then((res)=>{
+    console.log("Promise.all()",res);
+}).catch((err)=>{
+    console.log("Promise.all()",err);
+});
+
+Promise.allSettled([promise1,promise2,promise3]).then((res)=>{
+    console.log("Promise.allsettled",res)
+}).catch((err)=>{
+    console.log("Promise.allsettled",err)
+});
+
+
+//Pormise.race()
+
+Promise.race([promise1,promise2,promise3]).then((res)=>
+    console.log("Promise.race",res)
+).catch((err)=>{
+    console.log("Promise.allsettled",err)
+});
 
 //pollyfills for Promise.all()
 
